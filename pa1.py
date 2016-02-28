4a5cc2dec2d7c8661fab746580b64845ebb57ff0
Alexander Iansiti <aiansiti@outlook.com>
2016-02-28 09:02:58 -0500

    socket io prelim stuff

--- pa1.py
+++ pa1.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, flash, url_for, redirect, \
     render_template, abort, send_from_directory, jsonify
 app = Flask(__name__)
+from flask_socketio import SocketIO
+
+socketio = SocketIO(app)
+is_chaos = getchaos()
+user_count = 0
 
 
 @app.route("/")
@@ -10,3 +15,13 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+    socketio.run(app)
+
+@socketio.on("add user")
+def handle_add_user(json):
+
+
+@socketio.on("sendInput")
+def handle_input(json):
+    if(is_chaos):
+        handle_chaos(json)
