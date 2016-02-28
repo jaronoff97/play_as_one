53dd596a94d848fa85b3730817cab0842bc81e10
Peter Mango <wpeter.mango@gmail.com>
2016-02-28 08:44:50 -0500

    PlayAsOne: Add Mode Combobox

--- PlayAsOne.py
+++ PlayAsOne.py
@@ -10,6 +10,9 @@ class PlayAsOne:
         self.gui = GUI()
         self.gui.mainloop()
 
+    def take_game_screenshot(self):
+        pass
+
     def find_game_window(self):
         pass
 
@@ -23,3 +26,12 @@ class PlayAsOne:
 class GUI(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+
+        self.mode_frame = tk.Frame()
+        self.mode_frame.grid(row=0, column=0)
+
+        self.mode_label = tk.Label(self.mode_frame, text='Mode: ')
+        self.mode_label.grid(row=0, column=0)
+
+        self.mode_combobox = ttk.Combobox(self.mode_frame, state='readonly', width=9, values=('Chaos', 'Democracy'))
+        self.mode_combobox.grid(row=0, column=1)
