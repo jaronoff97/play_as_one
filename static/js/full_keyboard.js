0daea0e134cdd15d8e8aae56d9c323dece95dc68
jaronoff97 <jacobaronoff45@gmail.com>
2016-02-28 16:31:00 -0500

    Latest Commit from sublime text

--- static/js/full_keyboard.js
+++ static/js/full_keyboard.js
@@ -47,7 +47,7 @@ var $write = $('#write'),
 
             shift = false;
         }
-
+        socket.emit("sendInput",{user_input: character});
         // Add the character
         $write.html($write.html() + character);
     });
